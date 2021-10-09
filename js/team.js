a0440a963d849baaadfdb709788816623bbc97d6
Anmol Jhamb <codingwithaj@gmail.com>
2021-10-09 13:38:35 +0530

    Added Everything

---
+++ js/team.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#nav-placeholder").load("navbar.html");
+});
+
+$(function() {
+    $("#timeline-placeholder").load("timeline.html");
+});
+
+$(function() {
+    $("#footer-placeholder").load("footer.html");
+});
